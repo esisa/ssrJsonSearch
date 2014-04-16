c08202e70fd1324876d56b8a7bfe8abac417b044
root <root@beta.turkompisen.no>
2014-04-16 11:04:23 +0200

    Cleaned up code

--- ssrSok/app.py
+++ ssrSok/app.py
@@ -10,9 +10,7 @@ app = Flask(__name__)
 @app.route('/ssr')
 def ssrSok():
     query = request.args.get('q', '')
-    print 'https://ws.geonorge.no/SKWS3Index/ssr/sok?navn='+query+'*&antPerSide=9&epsgKode=4258&eksakteForst=true'
     r = requests.get('https://ws.geonorge.no/SKWS3Index/ssr/sok?navn='+query+'*&antPerSide=9&epsgKode=4258&eksakteForst=true', verify=False)
-    print r
     doc = xmltodict.parse(r.text)
     js = json.dumps(doc)
 
@@ -20,7 +18,7 @@ def ssrSok():
     return resp
 
 if __name__ == "__main__":
-    app.debug = True
+    app.debug = False
     app.run()
 
 
